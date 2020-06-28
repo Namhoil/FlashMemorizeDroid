da28f9f8b66abbc9dbae46243085652b231604ca
sinkatsu23 <watasi.no.atsui.sinkouseikatsu@gmail.com>
2020-06-29 00:27:49 +0900

    the function of video

--- app/src/main/java/me/timgu/flashmemorize/HelpVideo.java
+++ app/src/main/java/me/timgu/flashmemorize/HelpVideo.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
+import android.widget.ViewFlipper;
 
 public class HelpVideo extends AppCompatActivity {
 
@@ -15,6 +18,25 @@ public class HelpVideo extends AppCompatActivity {
         final VideoView videoView=(VideoView)findViewById(R.id.videoView);
         videoView.setVideoURI(Uri.parse("android.resource://"+this.getPackageName()+"/"+R.raw.video1));
         videoView.start();
+
+        Button butpre, butnext;
+        final ViewFlipper viewFlipper;
+
+        butpre=(Button)findViewById(R.id.btnpre);
+        butnext=(Button)findViewById(R.id.btnnext);
+        viewFlipper=(ViewFlipper)findViewById(R.id.viewflipper);
+
+        butpre.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v){
+               viewFlipper.showPrevious();
+           }
+        });
+
+        butnext.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                viewFlipper.showNext();
+            }
+        });
 
     }
 }
